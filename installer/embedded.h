301eb93a75a52898ffe91795bb6cfbdc745d5b5f
Matías Israelson <57065102+israpps@users.noreply.github.com>
2022-06-05 02:43:59 -0300

    lol

---
+++ installer/embedded.h
@@ -0,0 +1,81 @@
+#ifndef EMBEDDED_INCLUDED
+#define EMBEDDED_INCLUDED
+extern u8 opl_icn[];
+extern int size_opl_icn;
+//----------------------------------------//
+extern u8 opl_sys[];
+extern int size_opl_sys;
+//----------------------------------------//
+extern u8 opl_cfg[];
+extern int size_opl_cfg;
+//----------------------------------------//
+extern u8 opl_cfg1[];
+extern int size_opl_cfg1;
+//----------------------------------------//
+extern u8 opl_dualshock[];
+extern int size_opl_dualshock;
+//----------------------------------------//
+extern u8  opentuna_slims[];
+extern int size_opentuna_slims;
+//----------------------------------------//
+extern u8 opentuna_fats[];
+extern int size_opentuna_fats;
+//----------------------------------------//
+extern u8 opentuna_fat170[];
+extern int size_opentuna_fat170;
+//----------------------------------------//
+extern u8 exploit_sys[];
+extern int size_exploit_sys;
+//----------------------------------------//
+extern u8 FMCB_ELF[];
+extern int size_FMCB_ELF;
+//----------------------------------------//
+extern u8 FMCB_CNF[];
+extern int size_FMCB_CNF;
+//----------------------------------------//
+extern u8 CFG_ELF[];
+extern int size_CFG_ELF;
+//----------------------------------------//
+extern u8 boot_icn[];
+extern int size_boot_icn;
+//----------------------------------------//
+extern u8 boot_sys[];
+extern int size_boot_sys;
+//----------------------------------------//
+extern u8 ule_cnf[];
+extern int size_ule_cnf;
+//----------------------------------------//
+extern u8 FUNTUNA_USBD[];
+extern int size_FUNTUNA_USBD;
+//----------------------------------------//
+extern u8 FUNTUNA_USBHDFSD[];
+extern int size_FUNTUNA_USBHDFSD;
+//----------------------------------------//
+extern u8 poweroff_elf[];
+extern int size_poweroff_elf;
+//----------------------------------------//
+extern u8 opl_elf[];
+extern int size_opl_elf;
+//----------------------------------------//
+extern u8 ule_elf[];
+extern int size_ule_elf;
+//----------------------------------------//
+extern u8 apps_icn[];
+extern int size_apps_icn;
+//----------------------------------------//
+extern u8 apps_sys[];
+extern int size_apps_sys;
+
+// Embedded IOP drivers
+extern unsigned char SIO2MAN_irx[];
+extern unsigned int size_SIO2MAN_irx;
+//----------------------------------------//
+extern unsigned char PADMAN_irx[];
+extern unsigned int size_PADMAN_irx;
+//----------------------------------------//
+extern unsigned char MCMAN_irx[];
+extern unsigned int size_MCMAN_irx;
+//----------------------------------------//
+extern unsigned char MCSERV_irx[];
+extern unsigned int size_MCSERV_irx;
+#endif //EMBEDDED_INCLUDED
--- installer/main.c
+++ installer/main.c
@@ -11,91 +11,11 @@
 	#define DEBUG_PRINTF(arg...)
 #endif
 
-
-extern u8 opl_icn[];
-extern int size_opl_icn;
-//----------------------------------------//
-extern u8 opl_sys[];
-extern int size_opl_sys;
-//----------------------------------------//
-extern u8 opl_cfg[];
-extern int size_opl_cfg;
-//----------------------------------------//
-extern u8 opl_cfg1[];
-extern int size_opl_cfg1;
-//----------------------------------------//
-extern u8 opl_dualshock[];
-extern int size_opl_dualshock;
-//
-//----------------------------------------//
-extern u8  opentuna_slims[];
-extern int size_opentuna_slims;
-//----------------------------------------//
-extern u8 opentuna_fats[];
-extern int size_opentuna_fats;
-//----------------------------------------//
-extern u8 opentuna_fat170[];
-extern int size_opentuna_fat170;
-//----------------------------------------//
-extern u8 exploit_sys[];
-extern int size_exploit_sys;
-//----------------------------------------//
-extern u8 FMCB_ELF[];
-extern int size_FMCB_ELF;
-//----------------------------------------//
-extern u8 FMCB_CNF[];
-extern int size_FMCB_CNF;
-//----------------------------------------//
-extern u8 CFG_ELF[];
-extern int size_CFG_ELF;
-//----------------------------------------//
-extern u8 boot_icn[];
-extern int size_boot_icn;
-//----------------------------------------//
-extern u8 boot_sys[];
-extern int size_boot_sys;
-//----------------------------------------//
-extern u8 ule_cnf[];
-extern int size_ule_cnf;
-//----------------------------------------//
-extern u8 FUNTUNA_USBD[];
-extern int size_FUNTUNA_USBD;
-//----------------------------------------//
-extern u8 FUNTUNA_USBHDFSD[];
-extern int size_FUNTUNA_USBHDFSD;
-//----------------------------------------//
-extern u8 poweroff_elf[];
-extern int size_poweroff_elf;
-//----------------------------------------//
-extern u8 opl_elf[];
-extern int size_opl_elf;
-//----------------------------------------//
-extern u8 ule_elf[];
-extern int size_ule_elf;
-//----------------------------------------//
-extern u8 apps_icn[];
-extern int size_apps_icn;
-//----------------------------------------//
-extern u8 apps_sys[];
-extern int size_apps_sys;
-
-// Embedded IOP drivers
-extern unsigned char SIO2MAN_irx[];
-extern unsigned int size_SIO2MAN_irx;
-
-extern unsigned char PADMAN_irx[];
-extern unsigned int size_PADMAN_irx;
-
-extern unsigned char MCMAN_irx[];
-extern unsigned int size_MCMAN_irx;
-
-extern unsigned char MCSERV_irx[];
-extern unsigned int size_MCSERV_irx;
-
+#include "embedded.h"
 
 static int pad_inited = 0;
 int key;
-#include "BMPS/FUNTUNA_FORK_INSTALLER_BG.h"
+//#include "BMPS/FUNTUNA_FORK_INSTALLER_BG.h"
 #include "BMPS/FUNTUNA_FORK_INSTALLER_WELCOME.h"
 #include "BMPS/FUNTUNA_FORK_INSTALLER_SUCESS.h"
 #include "BMPS/FUNTUNA_FORK_INSTALLER_FAIL.h"
@@ -111,6 +31,7 @@ int key;
 #include "BMPS/NO_MEMORY_CARD.h"
 #include "BMPS/NO_PS2_MC.h"
 
+extern uint32 BG[];
 enum OPENTUNA_VARIANTS
 {
 	SLIMS = 0,	  // fat 0x190 and every 0x2?? ROM
