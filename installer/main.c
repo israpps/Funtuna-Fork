17aeac44665dc0b8e61fd321eceb0ad6e79b21a2
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-01-02 22:34:10 -0300

    initial commit

--- installer/main.c
+++ installer/main.c
@@ -2,100 +2,48 @@
 
 /*------------------------------------------------------------*/
 #include "main.h"
+#include <osd_config.h>
 //*/
 #ifdef GS_DEBUG_ISRA
 #define DEBUG_PRINTF(arg...) scr_printf(arg...);
 #else 
 #define DEBUG_PRINTF(arg...)	
 #endif
+#define IMPORT_BIN(_n) \
+extern u8 _n[]; \
+extern int size_##_n
 
-extern u8 opl_icn[];
-extern int size_opl_icn;
-//----------------------------------------//
-extern u8 opl_sys[];
-extern int size_opl_sys;
-//----------------------------------------//
-extern u8 opl_cfg[];
-extern int size_opl_cfg;
-//----------------------------------------//
-extern u8 opl_cfg1[];
-extern int size_opl_cfg1;
-//----------------------------------------//
-extern u8 opl_dualshock[];
-extern int size_opl_dualshock;
-//
-//----------------------------------------//
-extern u8  opentuna_slims[];
-extern int size_opentuna_slims;
-//----------------------------------------//
-extern u8 opentuna_fats[];
-extern int size_opentuna_fats;
-//----------------------------------------//
-extern u8 opentuna_fat170[];
-extern int size_opentuna_fat170;
-//----------------------------------------//
-extern u8 exploit_sys[];
-extern int size_exploit_sys;
-//----------------------------------------//
-extern u8 FMCB_ELF[];
-extern int size_FMCB_ELF;
-//----------------------------------------//
-extern u8 FMCB_CNF[];
-extern int size_FMCB_CNF;
-//----------------------------------------//
-extern u8 CFG_ELF[];
-extern int size_CFG_ELF;
-//----------------------------------------//
-extern u8 boot_icn[];
-extern int size_boot_icn;
-//----------------------------------------//
-extern u8 boot_sys[];
-extern int size_boot_sys;
-//----------------------------------------//
-extern u8 ule_cnf[];
-extern int size_ule_cnf;
-//----------------------------------------//
-extern u8 FUNTUNA_USBD[];
-extern int size_FUNTUNA_USBD;
-//----------------------------------------//
-extern u8 FUNTUNA_USBHDFSD[];
-extern int size_FUNTUNA_USBHDFSD;
-//----------------------------------------//
-extern u8 poweroff_elf[];
-extern int size_poweroff_elf;
-//----------------------------------------//
-extern u8 opl_elf[];
-extern int size_opl_elf;
-//----------------------------------------//
-extern u8 ule_elf[];
-extern int size_ule_elf;
-//----------------------------------------//
-extern u8 apps_icn[];
-extern int size_apps_icn;
-//----------------------------------------//
-extern u8 apps_sys[];
-extern int size_apps_sys;
+IMPORT_BIN(PS2BBL_CFG);
+IMPORT_BIN(PS2BBL_icn);
+IMPORT_BIN(PS2BBL_sys);
+IMPORT_BIN(PS2BBL_ELF);
+IMPORT_BIN(opl_icn);
+IMPORT_BIN(opl_sys);
+IMPORT_BIN(opl_cfg);
+IMPORT_BIN(opl_cfg1);
+IMPORT_BIN(opl_dualshock);
+IMPORT_BIN(opentuna_slims);
+IMPORT_BIN(opentuna_fats);
+IMPORT_BIN(opentuna_fat170);
+IMPORT_BIN(exploit_sys);
+IMPORT_BIN(boot_icn);
+IMPORT_BIN(boot_sys);
+IMPORT_BIN(opl_elf);
+IMPORT_BIN(ule_elf);
+IMPORT_BIN(apps_icn);
+IMPORT_BIN(apps_sys);
 
 static int pad_inited = 0;
-#include "FUNTUNA_FORK_INSTALLER_BG.h"
-#include "FUNTUNA_FORK_INSTALLER_WELCOME.h"
-#include "FUNTUNA_FORK_INSTALLER_SUCESS.h"
-#include "FUNTUNA_FORK_INSTALLER_FAIL.h"
 
-#include "ERROR_MESSAGES/INSTALL_QUERY.h"
-#include "ERROR_MESSAGES/ICONQUERY.h"
-#include "ERROR_MESSAGES/ICONQUERY_FAT170.h"
-#include "ERROR_MESSAGES/ICONQUERY_FATS.h"
-#include "ERROR_MESSAGES/ICONQUERY_SLIMS.h"
-
-#include "ERROR_MESSAGES/FOUND_APPS.h"
-#include "ERROR_MESSAGES/FOUND_BOOT.h"
-#include "ERROR_MESSAGES/FOUND_FORTUNA.h"
-#include "ERROR_MESSAGES/FOUND_FUNTUNA.h"
-#include "ERROR_MESSAGES/FOUND_OPENTUNA.h"
-#include "ERROR_MESSAGES/NOT_ENOUGH_SPACE.h"
-#include "ERROR_MESSAGES/NO_MEMORY_CARD.h"
-#include "ERROR_MESSAGES/NO_PS2_MC.h"
+#define RBG2INT(R,G,B) ((0<<24) + (R<<16) + (G<<8) + B)
+const char banner[] = {
+    "\t\t__________  _________________   ____________________.____\n"
+    "\t\t\\______   \\/   _____/\\_____  \\  \\______   \\______   \\    |\n"
+    "\t\t |     ___/\\_____  \\  /  ____/   |    |  _/|    |  _/    |\n"
+    "\t\t |    |    /        \\/       \\   |    |   \\|    |   \\    |___\n"
+    "\t\t |____|   /_______  /\\_______ \\  |______  /|______  /_______ \\\n"
+    "\t\t                  \\/         \\/         \\/        \\/        \\/\n"
+};
 
 enum OPENTUNA_VARIANTS
 {
@@ -260,6 +208,7 @@ static int DeleteFolder(const char *folder)
 //=============================================================
 static void InitPS2(void)
 {
+	printf("%s: start\n", __FUNCTION__);
 	ResetIOP();
 	SifInitIopHeap();
 	SifLoadFileInit();
@@ -326,8 +275,10 @@ static int write_embed_replace(void *embed_file, const int embed_size, char* fol
 //return 0 = ok, return 1 = error
 static int install(int mcport, int icon_variant)
 {
-	display_bmp(640, 448, BG);
+	scr_clear();
+	scr_setfontcolor(0x00ffff);
 	scr_printf("\n\n\nInstalling for memory card %u...\n",mcport);
+	scr_setfontcolor(0xffffff);
 	char version_manifest_path[32];
 	char opl_settings_location[32];
 	char opl_daily_bulshit_cnf[32];
@@ -357,6 +308,8 @@ static int install(int mcport, int icon_variant)
 		DeleteFolder(temp_path);
 	sprintf(temp_path,"mc%u:OPENTUNA", mcport);
 		DeleteFolder(temp_path);
+	sprintf(temp_path,"mc%u:PS2BBL", mcport);
+		DeleteFolder(temp_path);
 	sprintf(temp_path,"mc%u:BXEXEC-OPENTUNA", mcport);
 		DeleteFolder(temp_path);
 	
@@ -370,8 +323,8 @@ static int install(int mcport, int icon_variant)
 	
 	if (must_kill_opl_folder == 1)
 	{
-	sprintf(temp_path,"mc%u:OPL", mcport);
-	DeleteFolder(temp_path);
+		sprintf(temp_path,"mc%u:OPL", mcport);
+		DeleteFolder(temp_path);
 	}
 	// /CLEANUP
 	//If there's no free space, we have an error:
@@ -394,12 +347,17 @@ static int install(int mcport, int icon_variant)
 	if (file_exists("mc1:/FORTUNA/icon.sys"))         {return FORTUNA_FOUND;}
 	}
 	//FOLDERS
+	scr_setfontcolor(0x00ffff);
 	scr_printf("\tCreating Folders...\n");
+	scr_setfontcolor(0xffffff);
 	scr_printf("\t\tBOOT\n");
 		ret = mcMkDir(mcport, 0, "BOOT");
 		mcSync(0, NULL, &ret);
 	scr_printf("\t\tAPPS\n");
 		ret = mcMkDir(mcport, 0, "APPS");
+		mcSync(0, NULL, &ret);
+	scr_printf("\t\tPS2BBL\n");
+		ret = mcMkDir(mcport, 0, "PS2BBL");
 		mcSync(0, NULL, &ret);
 	scr_printf("\t\tOPL\n");
 		ret = mcMkDir(mcport, 0, "OPL");
@@ -410,7 +368,9 @@ static int install(int mcport, int icon_variant)
 		mcSync(0, NULL, &ret);	
 	}
 	///FOLDERS
+	scr_setfontcolor(0x00ffff);
 	scr_printf("\tWriting Files\n");
+	scr_setfontcolor(0xffffff);
 	//OPENTUNA
 	if (icon_variant != UNSUPPORTED) 
 	{
@@ -441,7 +401,9 @@ static int install(int mcport, int icon_variant)
 				ret = write(fd, ICONTYPE_ALIAS[icon_variant], 4);
 				close(fd);
 				}
+		scr_setfontcolor(0x00ffff);
 		scr_printf("\t\tManipulating Opentuna icon timestamp\n");
+		scr_setfontcolor(0xffffff);
 		static sceMcTblGetDir mcDirAAA[64] __attribute__((aligned(64)));
 		static sceMcStDateTime maximahora; //Maxium Timestamp, for the ones who does not speak Spanish
 	
@@ -457,51 +419,51 @@ static int install(int mcport, int icon_variant)
 		mcSetFileInfo(mcport, 0, "BXEXEC-OPENTUNA", mcDirAAA, 0x02);
 		mcSync(0, NULL, &ret);		
 				
-	} else {scr_printf("\t THIS PS2 IS INCOMPATIBLE WITH OPENTUNA\n\tSKIPPING OPENTUNA FILES!\n");DeleteFolder(temp_path);}
+	} else {
+	scr_setfontcolor(0x0000ff);
+	scr_printf("\t THIS PS2 IS INCOMPATIBLE WITH OPENTUNA\n\tSKIPPING OPENTUNA FILES!\n");DeleteFolder(temp_path);
+	scr_setfontcolor(0xffffff);
+	}
 	///OPENTUNA
 	
 	//FUNTUNA&APPS
+	scr_printf("\t\tPS2BBL\n");
+		retorno = write_embed(&PS2BBL_ELF, size_PS2BBL_ELF, "BOOT", "BOOT.ELF",mcport);
+		if (retorno < 0) {return 6;}
+
+	scr_printf("\t\tPS2BBL Configuration\n");
+		retorno = write_embed(&PS2BBL_CFG, size_PS2BBL_CFG, "PS2BBL", "CONFIG.INI",mcport);
+		if (retorno < 0) {return 6;}
+		retorno = write_embed(&PS2BBL_icn, size_PS2BBL_icn, "PS2BBL", "PS2BBL.icn",mcport);
+		if (retorno < 0) {return 6;}
+		retorno = write_embed(&PS2BBL_sys, size_PS2BBL_sys, "PS2BBL", "icon.sys",mcport);
+		if (retorno < 0) {return 6;}
+
 	scr_printf("\t\tuLaunchELF\n");
 		retorno = write_embed(&ule_elf, size_ule_elf, "BOOT", "ULE.ELF",mcport);
 		if (retorno < 0) {return 6;}
-		retorno = write_embed(&ule_cnf, size_ule_cnf, "BOOT", "LAUNCHELF.CNF",mcport);
-		if (retorno < 0) {return 6;}
-
-	scr_printf("\t\tFreeMcBoot\n");
-		retorno = write_embed(&FMCB_ELF, size_FMCB_ELF, "BOOT", "BOOT.ELF",mcport);
-		if (retorno < 0) {return 6;}
-		retorno = write_embed(&FMCB_CNF, size_FMCB_CNF, "BOOT", "FUNTUNA.CNF",mcport);
-		if (retorno < 0) {return 6;}
-	scr_printf("\t\tFreeMcBoot Configurator\n");
-		retorno = write_embed(&CFG_ELF, size_CFG_ELF, "BOOT", "CFG.ELF",mcport);
-		if (retorno < 0) {return 6;}
-        
-    scr_printf("\t\tFreeMcBoot USB drivers\n");
-		retorno = write_embed(&FUNTUNA_USBD, size_FUNTUNA_USBD, "BOOT", "USBD.IRX",mcport);
-		if (retorno < 0) {return 6;}
-                retorno = write_embed(&FUNTUNA_USBHDFSD, size_FUNTUNA_USBHDFSD, "BOOT", "USBHDFSD.IRX",mcport);
-		if (retorno < 0) {return 6;}
 
 	scr_printf("\t\tBOOT icons\n");
-		retorno = write_embed(&boot_icn, size_boot_icn, "BOOT", "SYSTEM.icn",mcport);
+		retorno = write_embed(&boot_icn, size_boot_icn, "BOOT", "PS2BBL.icn",mcport);
 		if (retorno < 0) {return 6;}
 		retorno = write_embed(&boot_sys, size_boot_sys, "BOOT", "icon.sys",mcport);
 		if (retorno < 0) {return 6;}
-	scr_printf("\t\tPoweroff utility\n");
-		retorno = write_embed(&poweroff_elf, size_poweroff_elf, "BOOT", "POWEROFF.ELF",mcport);
-		if (retorno < 0) {return 6;}
 	///FUNTUNA&APPS
-	scr_printf("\t\tAPPS folder icons\n");
+	scr_printf("\t\tAPPS icons\n");
 		retorno = write_embed(&apps_sys, size_apps_sys, "APPS","icon.sys",mcport);
 		if (retorno < 0) {return 6;}
-		retorno = write_embed(&apps_icn, size_apps_icn, "APPS","funtuna_apps.icn",mcport);
+		retorno = write_embed(&apps_icn, size_apps_icn, "APPS","APPS.icn",mcport);
 		if (retorno < 0) {return 6;}
+
 	scr_printf("\t\tOPL\n");
 		retorno = write_embed(&opl_elf, size_opl_elf, "APPS","OPNPS2LD.ELF",mcport);
 		if (retorno < 0) {return 6;}
+
 	if (!file_exists(opl_settings_location))//If no OPL config file...
 	{
+		scr_setfontcolor(0x00ffff);
 		scr_printf("\t\tNo OPL settings found!\n\t\t Loading Preconfigured OPL Folder...\n");
+		scr_setfontcolor(0xffffff);
 		if (mcport==0)
 			write_embed_replace(&opl_cfg, size_opl_cfg, "OPL", "conf_opl.cfg", mcport);//main config file has two variants, each of them has IGR Path assigned according to mcport value
 		else write_embed_replace(&opl_cfg1, size_opl_cfg1, "OPL", "conf_opl.cfg", mcport);//
@@ -554,75 +516,77 @@ static void CleanUp(void) //trimmed from FMCB
 void tell_the_user_wtf_happened(int retval)
 {
 	scr_clear();
+	scr_setfontcolor(0x0000ff);
+	scr_printf("\n\n\n\t\tERROR!\n\n\n");
+	scr_setfontcolor(0xffffff);
 	switch (retval)
 	{
 		case NO_MEMORY_CARD:
-			display_bmp(640, 448, _NO_MEMORY_CARD);
+			scr_printf("\tNo memory card detected!\n");
 			break;
 			
 		case NO_PS2_MEMORY_CARD:
-			display_bmp(640, 448, NO_PS2_MC);
+			scr_printf("\tNo PS2 memory card detected!\n");
 			break;
 			
 		case NOT_ENOUGH_SPACE:
-			display_bmp(640, 448, _NOT_ENOUGH_SPACE);
+			scr_printf("\tNot enough space on card!\n");
 			break;
 			
 		case FORTUNA_FOUND:
-			display_bmp(640, 448, FOUND_FORTUNA);
-			break;
-			
 		case OPENTUNA_FOUND:
-			display_bmp(640, 448, FOUND_OPENTUNA);
-			break;
-			
 		case FUNTUNA_FORK_FOUND:
-			display_bmp(640, 448, FOUND_FUNTUNA);
-			break;
-			
 		case BOOT_FOLDER_EXISTS:
-			display_bmp(640, 448, FOUND_BOOT);
-			break;
-			
 		case APPS_FOLDER_EXISTS:
-			display_bmp(640, 448, FOUND_APPS);
+			scr_printf("\tI/O error, conflictive folder could not be wiped.\n");
 			break;
 			
 		default:
-			display_bmp(640, 448, FAIL);
+		scr_setfontcolor(0x00ffff);
+			scr_printf("\tUnknown error!\n");
+		scr_setfontcolor(0xffffff);
 	}
-	scr_printf("\n\n\nERROR CODE: [%d]",retval);
+	scr_printf("\t\nERROR CODE: [%d]",retval);
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 int IconQuery(int default_icon_type)
 {
-	switch (default_icon_type)
-	{
-		case SLIMS:
-			display_bmp(640, 448, ICONQUERY_SLIMS);
-			break;
-		case FATS:
-			display_bmp(640, 448, ICONQUERY_FATS);
-			break;
-		case FAT170:
-			display_bmp(640, 448, ICONQUERY_FAT170);
-			break;
-		default:
-			display_bmp(640, 448, ICONQUERY);
-	}
-	
+	scr_clear();
+	scr_setfontcolor(0x00ffff);
+	scr_printf("\n\n\n\t\t\tChoose Icon variant\n\n\t(the one compatible with this ps2 will have a green tint)\n\n");
+	scr_setfontcolor(0xffffff);
+	if (default_icon_type == SLIMS)
+		scr_setfontcolor(0x00ff00);
+
+	scr_printf("\tTRIANGLE: All Slims and SCPH-500xx with 1.90 ROM\n");
+	scr_setfontcolor(0xffffff);
+
+	if (default_icon_type == FATS)
+		scr_setfontcolor(0x00ff00);
+
+	scr_printf("\tCROSS: FATs from SCPH-18000 to SCPH-390xx (ROM 1.10 to 1.60)\n");
+	scr_setfontcolor(0xffffff);
+
+	if (default_icon_type == FAT170)
+		scr_setfontcolor(0x00ff00);
+
+	scr_printf("\tSQUARE: SCPH-500xx with 1.70 ROM\n");
+	scr_setfontcolor(0xffffff);
+
 	while (1)
 	{
 		readPad();
 		if (new_pad & PAD_TRIANGLE)     return SLIMS;
 		if (new_pad & PAD_CROSS)  		return FATS;
 		if (new_pad & PAD_SQUARE) 		return FAT170;
-
 	}
 }
 int Choose_Install_Mode(int default_icon_type)
 {
-	display_bmp(640, 448, INSTALL_QUERY);
+	scr_clear();
+	scr_printf("\n\n\n\tChoose installation mode:\n");
+	scr_printf("\tL2: Manual install\n");
+	scr_printf("\tR2: Automatic install\n");
 	while (1)
 	{
 		readPad();
@@ -639,9 +603,33 @@ static void PS2_browser(void)
 	LoadExecPS2("rom0:OSDSYS", 0, NULL);
 }
 
-int main (int argc, char *argv[])
+void inst_success(void)
 {
-	
+	scr_clear();
+	scr_setfontcolor(0x00ff00);
+	scr_printf("\n\n\n\t\tInstallation succeeded!\n\n\tPress start to quit\n\n");
+	scr_setfontcolor(0xffffff);
+
+	if (configGetLanguage() == 0)
+	{
+		scr_setfontcolor(0x0000ff);
+		scr_printf("CONSOLE LANGUAGE IS IN JAPANESE\n\tOPENTUNA CRASHES IF JAPANESE TEXT IS BEING DISPLAYED\n\t PLEASE CHANGE TO ANOTHER LANGUAGE BEFORE RUNNING OPENTUNA\n");
+		scr_setfontcolor(0xffffff);
+	}
+
+}
+void inst_err(void)
+{
+	scr_clear();
+	scr_setfontcolor(0x0000ff);
+	scr_printf("\n\n\n\t\tInstallation Failed!\n\n\tPress start to quit\n");
+	scr_setfontcolor(0xffffff);
+}
+
+int main(int argc, char *argv[])
+{
+	init_scr();
+	printf("PS2BBL+OpenTuna Installer starts!\n");
 	int fdn, icontype;
 	unsigned long int ROM_VERSION;
 	char romver[5];
@@ -649,8 +637,8 @@ int main (int argc, char *argv[])
 
 	// Loads Needed modules
 	InitPS2();
-
-	gs_reset(); // Reset GS
+	printf("Reading ROMVER\n");
+	//gs_reset(); // Reset GS
 	if ((fdn = open("rom0:ROMVER", O_RDONLY)) > 0) // Reading ROMVER
 	{
 		read(fdn, romver, 4);
@@ -661,11 +649,12 @@ int main (int argc, char *argv[])
 		romver[4] = '\0';
 		ROM_VERSION = strtoul(romver, NULL, 16); //convert ROM version to unsigned long int for further use on automatic Install
 	}
-
+	/*
 	if (VMode == PAL)
 		gs_init(PAL_640_512_32);
 	else
-		gs_init(NTSC_640_448_32);
+		gs_init(NTSC_640_448_32);*/
+	printf("calculating required icon type\n");
 	icontype = GetIconType(ROM_VERSION);
 	waitAnyPadReady();
 	pad_inited = 1;
@@ -674,7 +663,9 @@ int main (int argc, char *argv[])
 	int menuactual = 101;//101: Initial Menu, 102: Installing (not needed), 103: Error, 104: Done
 	//printf("FunTuna Fork Installer\n\nPress L1 to install on memory card 1 | Press R1 to install on memory card 2");
 
-	display_bmp(640, 448, WELCOME);
+	scr_printf("\n\n\n\n%s\n\n\t\t\t\tPlayStation Basic BootLoader + OpenTuna Installer\n\t\tPS2BBL By Matias Israelson\n\t\tOpenTuna by Alex Parrado\n\n", banner);
+	scr_printf("\tL1: Install on Memory Card 1\n");
+	scr_printf("\tR1: Install on Memory Card 2\n");
 	while (1) {
 		readPad();
 
@@ -685,7 +676,7 @@ int main (int argc, char *argv[])
 			iz = install(mcport, Choose_Install_Mode(icontype));
 			if(iz == 0){
 				menuactual = 104;
-				display_bmp(640, 448, SUCESS);
+				inst_success();
 			} else {
 				menuactual = 103;//error
 				tell_the_user_wtf_happened(iz);
@@ -698,7 +689,7 @@ int main (int argc, char *argv[])
 			iz = install(mcport, Choose_Install_Mode(icontype));
 			if(iz == 0){
 				menuactual = 104;
-				display_bmp(640, 448, SUCESS);
+				inst_success();
 			} else {
 				menuactual = 103;//error
 				tell_the_user_wtf_happened(iz);
